b6a00ac84e4e42a2ca7600aed81de887b562e99a
imperfectcastle <bart.sakowski@gmail.com>
2020-06-18 12:01:10 -0500

    final

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,64 @@
 import React, { Component } from "react";
 
+
+const postURL = 'http://localhost:6001/transactions'
+
 class AddTransactionForm extends Component {
+
+  constructor() {
+    super()
+    this.state={
+      date: "",
+      description: "",
+      category: "",
+      amount: ""
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmit = (event) => { // this works, but it doesn't re-render
+    event.preventDefault()
+    console.log(this.state)
+
+    const reqObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    } 
+    fetch(postURL, reqObj)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+
+
   render() {
+    console.log(this.state)
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={this.handleChange} />
+            <input type="text" name="description" placeholder="Description" onChange={this.handleChange}  />
+            <input type="text" name="category" placeholder="Category" onChange={this.handleChange} />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={this.handleChange} 
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" type="submit" >
             Add Transaction
           </button>
         </form>
@@ -26,3 +68,6 @@ class AddTransactionForm extends Component {
 }
 
 export default AddTransactionForm;
+
+
+// 
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+
+const TransactionsList = (props) => {
+  // console.log(props.transactions)
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +21,9 @@ const TransactionsList = () => {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render Transactions here */}
+        {props.transactions.map(transObj => {
+          return <Transaction transaction={transObj} key={transObj.id} />
+        })}
       </tbody>
     </table>
   );
